538ad8a3b95ef71db9480be6e8ae345add5acb1f
ShargeW <charge.ptc@gmail.com>
2018-08-25 20:55:17 +0700

    First frontend

---
+++ src/main/java/com/prokaprint/developer/BeerController.java
@@ -0,0 +1,33 @@
+package com.prokaprint.developer.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class BeerController {
+    private BeerRepository beerRepository;
+
+    public BeerController(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @GetMapping("/good-beers")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public Collection<Beer> goodBeers() {
+        return beerRepository.findAll().stream()
+                .filter(this::isGreat)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isGreat(Beer beer) {
+        return !beer.getName().equals("Budweiser") &&
+                !beer.getName().equals("Coors Light") &&
+                !beer.getName().equals("PBR");
+    }
+}
